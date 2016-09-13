2afa0ebfdbf2ff1b3262e001a66402b18b589d57
gabrielarotver <gabrielarotver86@gmail.com>
2016-09-13 19:18:12 -0400

    C# First App Initial Commit
    Initial Commit

---
+++ MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            //return "This is my default action...";
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome(string name, int ID = 1)
+        {
+            //return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
